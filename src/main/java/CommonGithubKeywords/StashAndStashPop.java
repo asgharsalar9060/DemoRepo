f24e0a7c84599059fafb7acdaffb5e3a72444187
Asghar <salar2015151@gmail.com>
2021-06-22 16:33:13 -0400

    StashAndStashPop changed

---
+++ src/main/java/CommonGithubKeywords/StashAndStashPop.java
@@ -0,0 +1,14 @@
+package CommonGithubKeywords;
+
+public class StashAndStashPop {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public void test() {
+		System.out.println("local test method...");
+	}
+
+}
